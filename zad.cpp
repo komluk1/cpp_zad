893552e16522dcc47122980755648badc3f4f745
Łukasz Komosa <komluk@hotmail.com>
2017-11-11 17:18:44 +0100

    without compilation errors

--- zad.cpp
+++ zad.cpp
@@ -1,20 +1,34 @@
+#include <stdio.h>
+#include <math.h>
 #define W while
-main() {
-	int n,q,k,m,s,f,a, A[10];
-	scanf("%d %d %d %d",&n,&q,&k,&m);
-	s=n+1;
-	W(s)
-	    scanf("%d", A+--s);
-	W(k-q++){
-	    f=n+1;
-	    W(f)
-	        s += pow(q-1, --f) * A[f];
-	    s%=(int)pow(10,m);
-	    W(s) {
-	        a = s %10;
-	        f += a*a;
-	        s /= 10;
-	    }
-	    printf("%d\n",f);
-	}
+
+int main()
+{
+    int n, q, k, m, s, f, a, A[15];
+    printf("Podaj n, q, k, m:");
+    scanf("%d %d %d %d",&n,&q,&k,&m);
+    s = n + 1;
+    W(s)
+    {
+        printf("Podaj s: ");
+        scanf("%d", A + --s);
+    }
+
+    W(k)
+    {
+        q++;
+        k--;
+        f = n + 1;
+        W(f)
+        s += pow(q - 1, --f) * A[f];
+        s %= (int)pow(10, m);
+        W(s)
+        {
+            a = s % 10;
+            f += a * a;
+            s /= 10;
+        }
+        printf("%d\n", f);
+    }
+    return 0;
 }
