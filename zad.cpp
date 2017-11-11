b5b3ca9a46ac8f98cd7bed755e5345a91d127e31
Łukasz Komosa <komluk@hotmail.com>
2017-11-11 15:09:30 +0100

    solution1

--- zad.cpp
+++ zad.cpp
@@ -1,8 +1,21 @@
-#include <iostream>
 #include <stdlib.h>
 #include <math.h>
 
-int main()
-{
-    printf("Chane in main cpp!");
+void main() {
+	int n,q,k,m,i,s,f,r,a, *A;
+	scanf("%d %d %d %d",&n,&q,&k,&m);
+	A = malloc (4*(n+1));
+	for(i=n;i>=0;)
+	    scanf("%d", &A[i--]);
+	for(i=q-1;m-i++;){
+	    r=f=s=0;
+	    for(;f<=n;)
+	        s += pow(i, f) * A[f++];
+	    while (s) {
+	        a = s %10;
+	        r += a*a;
+	        s /= 10;
+	    }
+	    printf("%d\n",r);
+	}
 }
