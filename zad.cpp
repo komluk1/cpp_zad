14dcfa6e5955a4496bd252f7b66f728d5b4c0cc8
Łukasz Komosa <komluk@hotmail.com>
2017-11-17 21:20:06 +0100

    solution 3

--- zad.cpp
+++ zad.cpp
@@ -1,34 +1,46 @@
 #include <stdio.h>
 #include <math.h>
-#define W while
+// #define W while
 
 int main()
 {
-    int n, q, k, m, s, f, a, A[15];
-    printf("Podaj n, q, k, m:");
-    scanf("%d %d %d %d",&n,&q,&k,&m);
-    s = n + 1;
-    W(s)
+    int n, q, k, m, i, r, s, f, a, A[10];
+    scanf("%d %d %d %d", &n, &q, &k, &m);
+    for (i = n; i >= 0;)
+        scanf("%d", &A[i--]);
+    for (i = q - 1; m - i++;)
     {
-        printf("Podaj s: ");
-        scanf("%d", A + --s);
-    }
-
-    W(k)
-    {
-        q++;
-        k--;
-        f = n + 1;
-        W(f)
-        s += pow(q - 1, --f) * A[f];
-        s %= (int)pow(10, m);
-        W(s)
+        r = f = s = 0;
+        while (f <= n)
+            s += pow(i, f) * A[f++];
+        while (s)
         {
             a = s % 10;
-            f += a * a;
+            r += a * a;
             s /= 10;
         }
-        printf("%d\n", f);
+        printf("%d\n", r);
     }
+    // int n, q, k, m, s, f, a, A[10];
+    // scanf("%d %d %d %d", &n, &q, &k, &m);
+    // s = n + 1;
+    // W(s)
+    //     scanf("%d", A + --s);
+    // W(k){
+    //     q++;
+    //     k--;
+    //     f = n + 1;
+    //     W(f){
+    //        s += pow(q - 1, --f) * A[f];
+    //        s %= (int)round(pow(10, m));
+    //     }
+    //     W(s)
+    //     {
+    //         a = s % 10;
+    //         f += a * a;
+    //         s /= 10;
+    //     }
+    //     printf("%d\n", f);
+    // }
     return 0;
 }
